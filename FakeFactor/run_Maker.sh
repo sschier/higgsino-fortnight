6d48248389fd50a3723f1f257741e732b49d8e08
sschier <sheena.schier@cern.ch>
2017-06-19 15:31:03 -0700

    ci

--- FakeFactor/run_Maker.sh
+++
@@ -1,7 +0,0 @@
-#python FFMaker.py --test --input "/export/home/sschier/workarea/tree_trimmer/data311481_skim.root" --isData --tree "CollectionTree" --outfile "outputHist_311481.root"
-#python FFMaker.py --test --input "/export/share/data/sschier/FakeLepton/1lepskims/data16/data310691_skim.root" --isData "data16" --tree "CollectionTree" --outfile "outputHist_310691.root"
-#python FFMaker.py --test --input "/export/share/data/sschier/FakeLepton/1lepskims/data15/data280319_skim.root" --isData "data15" --tree "CollectionTree" --outfile "outputHist_280319.root"
-#python FFMaker.py --test --input "/export/share/data/sschier/FakeLepton/1lepskims/data16/data310691_skim.root" --isData "data16" --tree "CollectionTree" --outfile "outputHist_310691.root"
-#python FFMaker.py --test --input "/export/share/data/sschier/FakeLepton/skims/data15_skims/data280319_skim.root" --isData "data15" --tree "CollectionTree" --outfile "outputHist_280319.root"
-python FFMaker.py --test --input "/export/share/data/sschier/FakeLepton/trigger_skims/wjets_skim/wjets361101_skim.root" --tree "wjets_22" --outfile "outputHist_361101.root"
-#python FFMaker.py --test --input "/export/share/data/sschier/FakeLepton/1lepskims/ttbar/ttbar410000_skim.root" --tree "ttbar" --outfile "outputHist_410000.root"
